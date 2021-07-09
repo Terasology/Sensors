53ae2734d80e289a41c4640716602d08e28525f0
DarkWeird <foxflameless@gmail.com>
2021-07-09 16:53:18 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components.

--- src/main/java/org/terasology/sensors/SensorComponent.java
+++ src/main/java/org/terasology/sensors/SensorComponent.java
@@ -1,19 +1,24 @@
-// Copyright 2020 The Terasology Foundation
+// Copyright 2021 The Terasology Foundation
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.sensors;
 
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.entitySystem.entity.EntityRef;
 import org.terasology.engine.network.Replicate;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 /**
  * A component representing a sensor entity. The sensor entity should contain trigger volumes for the detection of other
  * entities within a specified volume.
  */
-public class SensorComponent implements Component {
+public class SensorComponent implements Component<SensorComponent> {
     /**
      * The entity utilising this sensor.
      */
     @Replicate
     public EntityRef physicalSensor = EntityRef.NULL;
+
+    @Override
+    public void copy(SensorComponent other) {
+        this.physicalSensor = other.physicalSensor;
+    }
 }
--- src/main/java/org/terasology/sensors/volumeSensing/ConeSensorComponent.java
+++ src/main/java/org/terasology/sensors/volumeSensing/ConeSensorComponent.java
@@ -1,16 +1,21 @@
-// Copyright 2020 The Terasology Foundation
+// Copyright 2021 The Terasology Foundation
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.sensors.volumeSensing;
 
-import org.terasology.engine.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 /**
  * When this component is present on an entity, then it becomes capable of detecting other entities within a fixed cone
  * relative to its forward direction. The size of the cone is determined by its aperture.
  */
-public class ConeSensorComponent implements Component {
+public class ConeSensorComponent implements Component<ConeSensorComponent> {
     /**
      * The aperture of the cone, defined in degrees.
      */
     float aperture = 60.0f;
+
+    @Override
+    public void copy(ConeSensorComponent other) {
+        this.aperture = other.aperture;
+    }
 }
